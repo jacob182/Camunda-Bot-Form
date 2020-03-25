f661e7ca61e7a42c503ce0eeb7e2d2d4c7a44721
Jacob Coorey <n10088733@qut.edu.au>
2020-03-25 20:17:00 +1000

    Transfer of Camunda Bot Windows Form Application

---
+++ CamundaBotForm/CamundaBotForm/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+
+namespace CamundaBotForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            if (fbd.ShowDialog() == DialogResult.OK)
+                FilePathLbl.Text = (fbd.SelectedPath);
+        }
+
+
+        [STAThread]
+        private async void RunProgramBtn_Click(object sender, EventArgs e)
+        {
+            // * The definition key and ID are required for the CamundaBot to know which process to work with. *
+
+            // The definition key of the process.
+            string definitionKey = DefinitionKeyTxtBox.Text;
+
+            // The definition ID for your deployment of the process in Camunda.
+            string definitionID = DefinitionIDTxtBox.Text;
+
+            // The JSON object containing the required variables (Leave blank if none are required)
+            string json = JSONTxtBox.Text;
+
+            // Do you want to start the process again? (true/false)
+            bool startProcess = StartProcessCheckBox.Checked;
+            // if so, how many times?
+            int timesStarted = Convert.ToInt32(Math.Round(TimesTxtBox.Value, 0));
+
+
+            // * Setting the file name and file path for the purpose of saving the returned activities into a csv file *
+
+            // Where do you want to save the file?
+            string fileName = FileNameTxtBox.Text;  // the name of the file to be saved.
+
+            string filePath = FilePathLbl.Text; // This is where the file will be saved.
+
+            filePath = filePath + @"\" + fileName + ".csv"; // Joining them together for use.
+            
+
+            // ******* These parameters need to be configured correctly before running the bot *******
+
+            // Building the CamundaBot using the parameters above.
+            CamundaBot sequenceBot = new CamundaBot(definitionKey, definitionID, json);
+            
+
+            // Starting the process the required amount of times
+            try
+            {
+                if (startProcess)
+                {
+                    for (int i = 0; i < timesStarted; i++)
+                    {
+                        await sequenceBot.StartProcess();
+                    }
+                }
+            }
+            catch (HttpRequestException) // There was a problem connecting with the host.
+            {
+                string message = "No connection to the host could be made. Check the URL is correct, or the host is running.";
+                string caption = "Error While Starting Process";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch (NullReferenceException) // There was no response from the host after connection was made.
+            {
+                string message = "No connection to the host could be made. Check the URL is correct, or the host is running.";
+                string caption = "Error While Starting Process";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch(RestException)
+            {
+                string message = "No matching Process Definition matching key: " + definitionKey;
+                string caption = "Error While Starting Process";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+
+            // Create a list of the Activities from Camunda using the GetActivities function
+            List<Activity> activities = null;
+            try
+            {
+                activities = await sequenceBot.GetActivities();
+            }
+            catch (HttpRequestException) // There was a problem connecting with the host.
+            {
+                string message = "No connection to the host could be made. Check the URL is correct, or the host is running.";
+                string caption = "Error While Retrieving Activity Data";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch (NullReferenceException) // There was no response from the host after connection was made.
+            {
+                string message = "No data was received from the host. Check the Definition ID and Key are correct, or that the process has been started.";
+                string caption = "Error While Retrieving Activity Data";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            // Store the list of activities as a csv file.
+            StreamWriter writer;
+            CsvWriter csv;
+            try
+            {
+                using (writer = new StreamWriter(filePath))
+
+                    try
+                    {
+                        using (csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                        {
+                            try
+                            {
+                                csv.WriteRecords(activities);
+                            }
+                            catch (CsvHelper.WriterException)
+                            {
+                                string message = "No data was received from the host";
+                                string caption = "Error Saving Data";
+                                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+
+                        }
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        string message = "There was an issue while creating the file.";
+                        string caption = "Error Saving Data";
+                        MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+            }
+            catch (IOException)
+            {
+                string message = "The file path is incorrect or the file is still in use.";
+                string caption = "Error Saving Data";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Displays success label
+            SuccessLbl.Visible = true;
+        }
+
+        private void TimesTxtBox_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CamundaBotForm/CamundaBotForm/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
+
+namespace CamundaBotForm
+{
+        class Activity
+        {
+            public string id { get; set; }
+            public string parentActivityInstanceId { get; set; }
+            public string activityId { get; set; }
+            public string activityName { get; set; }
+            public string activityType { get; set; }
+            public string processDefinitionKey { get; set; }
+            public string processDefinitionId { get; set; }
+            public string processInstanceId { get; set; }
+            public string executionId { get; set; }
+            public string taskId { get; set; }
+            public string calledProcessInstanceId { get; set; }
+            public string calledCaseInstanceId { get; set; }
+            public string assignee { get; set; }
+            public string canceled { get; set; }
+            public string completeScope { get; set; }
+            public string tenantId { get; set; }
+            public string removalTime { get; set; }
+            public string rootProcessInstanceId { get; set; }
+            public string startTime { get; set; }
+            public string endTime { get; set; }
+            public int durationInMillis { get; set; }
+        }
+
+        public class RestException : Exception
+    {
+        public RestException()
+        {
+
+        }
+        public RestException(string message) : base(message)
+        {
+
+        }
+        public RestException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+
+
+
+
+        class CamundaBot
+        {
+            // The CamundaBot uses the definition key and ID to tell Camunda which process we are working with.  Each CamundaBot has a HttpClient that it uses to interact with the REST API of Camunda.
+            // The defnition key is used to start the process, and the definition ID is used to return the activity data that Camunda stores after starting a process.
+            private string definitionKey, definitionID, json;
+            private HttpClient client;
+
+
+            public CamundaBot(string definitionKey, string definitionID, string json)
+            {
+                this.definitionID = definitionID;
+                this.definitionKey = definitionKey;
+                this.json = json;
+                this.client = new HttpClient();
+            }
+
+
+
+            public async Task<string> StartProcess()
+            // This current version of start process assumes you have Camunda running on the local machine, as it uses the localhost address to cammunicate with the REST API.
+            // Start the process of the CamundaBot using the stored definitionId and json
+            {
+                string url = "http://localhost:8080/engine-rest/process-definition/key/" + this.definitionKey + "/start";
+
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                
+
+                // Declaring the variables to be used in starting the process
+                HttpResponseMessage response = new HttpResponseMessage();
+                string result = null;
+
+                // Send the POST message to Camunda to start the process
+                try
+                {
+                    response = await this.client.PostAsync(url, data);
+                }
+                catch (HttpRequestException ex)
+                {
+                    //Console.WriteLine("Error while starting process: No connection to the host could be made. Check the URL is correct, or the host is running.\n");
+                    throw ex;
+                }
+
+                // Retrieve the HTTP response message from Camunda to determine if the process started correctly.
+                try
+                {
+                    result = response.Content.ReadAsStringAsync().Result;
+                }
+                catch (NullReferenceException ex)
+                {
+                    //Console.WriteLine("Error while reading response from Camunda: No response has been received from the host.\n");
+                    throw ex;
+                }
+                Regex regex = new Regex("RestException");
+                Match match = regex.Match(result);
+            if (match.Success)
+            {
+                throw new RestException("No matching Process Definition matching the given key.");
+            }
+            
+
+                return result;
+            }
+
+
+
+
+            public async Task<List<Activity>> GetActivities()
+            {
+                string url = "http://localhost:8080/engine-rest/history/activity-instance?processDefinitionId=" + this.definitionID;
+
+                
+
+                HttpResponseMessage response = null;
+
+                // Retrieve the activity data from Camunda using a http GET request
+                try
+                {
+                    response = await this.client.GetAsync(url);
+                }
+                catch (HttpRequestException ex)
+                {
+                    //Console.WriteLine("Error while retrieving activity data: No connection to the host could be made. Check the URL is correct, or the host is running.\n");
+                    throw ex;
+                }
+
+                string[] activities = null;
+                try
+                {
+                    activities = Regex.Matches(response.Content.ReadAsStringAsync().Result, @"{[^{}]*}")
+                        .Cast<Match>()
+                        .Select(m => m.Value)
+                        .ToArray();
+                }
+                catch (NullReferenceException ex)
+                {
+                    //Console.WriteLine("Error while retrieving activity data: No data was received from the host.");
+                    throw ex;
+                }
+
+                
+
+                    List<Activity> activityList = new List<Activity>();
+
+                try
+                {
+                    foreach (string activity in activities)
+                    {
+                        activityList.Add(JsonConvert.DeserializeObject<Activity>(activity));
+                    }
+                }
+                catch (NullReferenceException ex) // No activities were retrieved. Create an exception to be handled.
+                {
+                    //Console.WriteLine("Error while retrieving activity data: No data was received from the host.");
+                    throw ex;
+                }
+                if (activityList.Count() == 0)
+                {
+                    throw new NullReferenceException();
+                }
+                
+                    return activityList;
+                }
+
+
+
+
+        }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        
+        class Program
+        { 
+        [STAThread]
+        static void Main(string[] args)
+            {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            }
+        }
+}
